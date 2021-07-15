8f478bb3ece4977643ba9bf9f08668306b2a0cd7
wlgn8648 <wlgn8648@gmail.com>
2021-07-15 15:44:51 +0900

    commit

--- GitHub/fastapi/main.py
+++ GitHub/fastapi/main.py
@@ -1,18 +1,37 @@
 from typing import Optional
-
-from fastapi import FastAPI
-
-from bs4 import BeautifulSoup
-import requests
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def func():
+    return "Hello World"
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_items(
+        item_id: int = Path(... , title="The ID of the item to get"),
+        q : Optional[str] = Query(None, alias="item-query")
+):
+    results = {"item_id" : item_id}
+    if q:
+        results.update({"q" : q})
+    return results
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price+item.tax
+        item_dict.update({"price_with_tax" : price_with_tax})
+    return item_dict
